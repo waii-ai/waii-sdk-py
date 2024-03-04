0197c7b50a36c796d2a04926b4642dd29e89330f
pravin-waii <pravin.mishra@waii.com>
2024-03-04 11:39:19 +0530

    Create Waii class and remove old static WAII class

--- waii_sdk_py/waii_sdk_py.py
+++ waii_sdk_py/waii_sdk_py.py
@@ -4,15 +4,19 @@ from .database import Database
 from .semantic_context import SemanticContext
 from .waii_http_client import WaiiHttpClient
 
-class WAII:
-    History = History
+
+
+class Waii:
+    History=History
     SemanticContext = SemanticContext
     Query = Query
     Database = Database
 
-    @staticmethod
-    def initialize(url: str = 'https://tweakit.waii.ai/api/', api_key: str = ''):
-        WaiiHttpClient.get_instance(url, api_key)
+    def initialize(self,url: str = 'https://tweakit.waii.ai/api/', api_key: str = '' ):
+        WaiiHttpClient.get_instance(url,api_key)
         conns = Database.get_connections().connectors
         if len(conns) > 0:
             Database.activate_connection(conns[0].key)
+
+
+WAII = Waii()
