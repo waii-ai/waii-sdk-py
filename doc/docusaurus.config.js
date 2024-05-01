4048879273c3579c67804e480a923e4520d65bd2
Gunther Hagleitner <g@waii.ai>
2024-05-01 14:10:49 -0700

    fix redirects

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -2,105 +2,98 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 (module.exports = {
-    title: 'Waii Pythont SDK',
-    tagline: 'World most accurate text-2-sql API',
-    url: 'https://doc.waii.ai',
-    baseUrl: '/python/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    organizationName: 'waii',
-    projectName: 'waii-sdk-py',
-    
-    presets: [
-	[
-	    '@docusaurus/preset-classic',
-	    ({
-		docs: {
-		    sidebarPath: require.resolve('./sidebars.js'),
-		    editUrl: 'https://github.com/waii-ai/waii-sdk-js/tree/main/docs/docs-waii-sdk-py',
-		},
-		theme: {
-		    customCss: require.resolve('./src/css/custom.css'),
-		},
-	    }),
+	title: 'Waii Pythont SDK',
+	tagline: 'World most accurate text-2-sql API',
+	url: 'https://doc.waii.ai',
+	baseUrl: '/python/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'waii',
+	projectName: 'waii-sdk-py',
+
+	presets: [
+		[
+			'@docusaurus/preset-classic',
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					editUrl: 'https://github.com/waii-ai/waii-sdk-js/tree/main/docs/docs-waii-sdk-py',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
 	],
-    ],
-    
-    themeConfig:
-    ({
-	navbar: {
-            title: 'Waii Python SDK',
-            logo: {
-		alt: 'Waii Logo',
-		src: 'img/logo.png',
-            },
-	    items: [
+
+	themeConfig:
+		({
+			navbar: {
+				title: 'Waii Python SDK',
+				logo: {
+					alt: 'Waii Logo',
+					src: 'img/logo.png',
+				},
+				items: [
 					{
-						type: 'dropdown',
-						label: 'Select Documentation',
-						position: 'right',
+						label: 'TypeScript/JavaScript SDK',
+						href: 'https://doc.waii.ai/js/docs/intro',
+					},
+					{
+						label: 'Python SDK',
+						href: 'https://doc.waii.ai/python/docs/intro',
+					},
+					{
+						label: 'CLI',
+						href: 'https://doc.waii.ai/cli/docs/intro',
+					},
+					{
+						label: 'Deployment & Architecture',
+						href: 'https://doc.waii.ai/deployment/docs/intro',
+					}
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Company',
 						items: [
 							{
-								label: 'TypeScript/JavaScript SDK',
-								to: 'https://doc.waii.ai/js/',
+								label: 'Website',
+								href: 'https://waii.ai/',
 							},
 							{
-								label: 'Python SDK',
-								to: 'https://doc.waii.ai/python/',
-							},
-							{
-								label: 'CLI',
-								to: 'https://doc.waii.ai/cli/',
-							},
-							{
-								label: 'Deployment & Architecture',
-								to: 'https://doc.waii.ai/deployment/',
+								label: 'LinkedIn',
+								href: 'https://www.linkedin.com/company/96891121/',
 							}
 						],
 					},
-	    ],
-	},
-	footer: {
-            style: 'dark',
-            links: [
-		{
-		    title: 'Company',
-		    items: [
-			{
-			    label: 'Website',
-			    href: 'https://waii.ai/',
+					{
+						title: 'Community',
+						items: [
+							{
+								label: 'Slack',
+								href: 'https://join.slack.com/t/waiicommunity/shared_invite/zt-1xib44mr5-LBa7ub9t_vGvo66QtbUUpg',
+							}
+						],
+					},
+					{
+						title: 'More',
+						items: [
+							{
+								label: 'GitHub',
+								href: 'https://github.com/waii-ai/waii-sdk-py',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Waii, Inc.`,
 			},
-			{
-			    label: 'LinkedIn',
-			    href: 'https://www.linkedin.com/company/96891121/',
-			}
-		    ],
-		},
-		{
-		    title: 'Community',
-		    items: [
-			{
-			    label: 'Slack',
-			    href: 'https://join.slack.com/t/waiicommunity/shared_invite/zt-1xib44mr5-LBa7ub9t_vGvo66QtbUUpg',
-			}
-		    ],
-		},
-		{
-		    title: 'More',
-		    items: [
-			{
-			    label: 'GitHub',
-			    href: 'https://github.com/waii-ai/waii-sdk-py',
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
 			},
-		    ],
-		},
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} Waii, Inc.`,
-	},
-	prism: {
-            theme: lightCodeTheme,
-            darkTheme: darkCodeTheme,
-	},
-    }),
+		}),
 });
