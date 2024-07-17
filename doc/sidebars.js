5aee78cae8cd622aa889cd3fc859b61759dcc82e
Wangda Tan <wangda@apache.org>
2024-07-16 17:41:58 -0700

    Updated sidebar

--- doc/sidebars.js
+++ doc/sidebars.js
@@ -1,53 +1,63 @@
 module.exports = {
     docs: [
-	{
-	    type: 'doc',
-	    id: 'intro',
-	    label: 'Introduction',
-	},
-	{
-	    type: 'doc',
-	    id: 'install',
-	    label: 'Installation'
-	},
-	{
-	    type: 'doc',
-	    id: 'getting-started',
-	    label: 'Getting Started',
-	},
-	{
-	    type: 'category',
-	    label: 'Notebooks',
-	    items: [
-		'colab',
-		'notebook',
-	    ],
-	},
-	{
-	    type: 'category',
-	    label: 'Modules',
-	    items: [
-		{
-		    type: 'doc',
-		    id: 'sql-query-module',
-		    label: 'SQL Query',
-		},
-		{
-		    type: 'doc',
-		    id: 'semantic-context-module',
-		    label: 'Semantic Context',
-		},
-		{
-		    type: 'doc',
-		    id: 'database-module',
-		    label: 'Database',
-		},
-		{
-		    type: 'doc',
-		    id: 'history-module',
-		    label: 'History',
-		},
-	    ],
-	},
+        {
+            type: 'doc',
+            id: 'intro',
+            label: 'Introduction',
+        },
+        {
+            type: 'doc',
+            id: 'install',
+            label: 'Installation'
+        },
+        {
+            type: 'doc',
+            id: 'getting-started',
+            label: 'Getting Started',
+        },
+        {
+            type: 'category',
+            label: 'Notebooks',
+            items: [
+                'colab',
+                'notebook',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Modules',
+            items: [
+                {
+                    type: 'doc',
+                    id: 'sql-query-module',
+                    label: 'SQL Query',
+                },
+                {
+                    type: 'doc',
+                    id: 'semantic-context-module',
+                    label: 'Semantic Context',
+                },
+                {
+                    type: 'doc',
+                    id: 'database-module',
+                    label: 'Database',
+                },
+                {
+                    type: 'doc',
+                    id: 'history-module',
+                    label: 'History',
+                },
+                {
+                    type: "doc",
+                    id: "chart-module",
+                    label: "Chart"
+                },
+				{
+					type: 'doc',
+					id: 'chat-module',
+					label: 'Chat'
+				}
+            ],
+        },
     ],
 };
