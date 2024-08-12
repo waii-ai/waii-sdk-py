52e86323a7b6c491b4de2737c143edeaa4d2c3ff
pravin-waii <pravin.mishra@waii.com>
2024-08-12 14:09:25 +0530

    Write manage user tests

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -1,7 +1,9 @@
 import unittest
 
 from waii_sdk_py import WAII
-from waii_sdk_py.user import CreateAccessKeyRequest, DelAccessKeyRequest, DelAccessKeyResponse, GetAccessKeyRequest
+from waii_sdk_py.user import CreateAccessKeyRequest, DelAccessKeyRequest, DelAccessKeyResponse, GetAccessKeyRequest, \
+    GetUserInfoRequest, GetUserInfoResponse, UpdateConfigRequest, User, CreateUserRequest, UserDTO, DeleteUserRequest, \
+    CommonResponse, ListUsersRequest, UpdateUserRequest
 
 
 class TestUser(unittest.TestCase):
@@ -58,6 +60,64 @@ class TestUser(unittest.TestCase):
 
         params = DelAccessKeyRequest(names=["test2"])
         resp = WAII.user.delete_access_key(params)
+
+    def test_get_user_info(self):
+        params = GetUserInfoRequest()
+        resp = WAII.user.get_user_info(params)
+        assert isinstance(resp, GetUserInfoResponse)
+
+    def test_update_config(self):
+        params = UpdateConfigRequest(updated={"key1": "value1", "key2": "value2"})
+        resp = WAII.user.update_config(params)
+        assert resp.configs['key1'] == 'value1'
+        assert resp.configs['key2'] == 'value2'
+        params = UpdateConfigRequest(deleted=["key1"], updated={"key3": "value3"})
+        resp = WAII.user.update_config(params)
+        assert 'key1' not in resp.configs
+        assert resp.configs['key3'] == 'value3'
+        params = UpdateConfigRequest()
+        resp = WAII.user.update_config(params)
+        assert resp.configs['key2'] == 'value2'
+        assert resp.configs['key3'] == 'value3'
+
+    def test_manage_user(self):
+        params = DeleteUserRequest(id = "user1")
+        try:
+            resp = WAII.user.delete_user(params)
+        except:
+            pass
+        params = CreateUserRequest(user=UserDTO( id="user1",name="Wangda Tan",tenant_id="my_tenant_id",org_id="my_org_id"))
+        resp = WAII.user.create_user(params)
+        assert isinstance(resp, CommonResponse)
+
+        params = ListUsersRequest(lookup_org_id = "my_org_id")
+        resp = WAII.user.list_users(params)
+        user1 = [user for user in resp.users if user.id == "user1"]
+        assert len(user1) > 0
+
+        params = UpdateUserRequest(user=UserDTO( id="user1",name="Pravin",tenant_id="my_tenant_id",org_id="my_org_id"))
+        resp = WAII.user.update_user(params)
+        assert isinstance(resp, CommonResponse)
+
+        params = ListUsersRequest(lookup_org_id="my_org_id")
+        resp = WAII.user.list_users(params)
+        user1 = [user for user in resp.users if user.id == "user1"]
+        assert user1[0].name == "Pravin"
+        
+        params = DeleteUserRequest(id="user1")
+        resp = WAII.user.delete_user(params)
+        assert isinstance(resp, CommonResponse)
+
+        params = ListUsersRequest(lookup_org_id="my_org_id")
+        resp = WAII.user.list_users(params)
+        user1 = [user for user in resp.users if user.id == "user1"]
+        assert len(user1) == 0
+
+
+
+
+
+
 
 
 
--- waii_sdk_py/user/user.py
+++ waii_sdk_py/user/user.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Dict, Any
 
 from waii_sdk_py.waii_http_client import WaiiHttpClient
 from pydantic import BaseModel
@@ -6,6 +6,14 @@ from pydantic import BaseModel
 LIST_ACCESS_KEY_ENDPOINT = "list-access-keys"
 DELETE_ACCESS_KEY_ENDPOINT = "delete-access-keys"
 CREATE_KEY_ENDPOINT = "create-key"
+GET_USER_INFO_ENDPOINT = "get-user-info"
+UPDATE_CONFIG_ENDPOINT = "update-config"
+CREATE_USER_ENDPOINT = "create-user"
+DELETE_USER_ENDPOINT = "delete-user"
+UPDATE_USER_ENDPOINT = "update-user"
+LIST_USERS_ENDPOINT = "list-users"
+
+
 
 
 class CreateAccessKeyRequest(BaseModel):
@@ -31,6 +39,55 @@ class DelAccessKeyResponse(BaseModel):
     pass
 
 
+class GetUserInfoRequest:
+    pass
+
+
+class GetUserInfoResponse(BaseModel):
+    id: str
+    name: str
+    email: str
+    roles: List[str]
+    permissions: List[str]
+
+
+class UpdateConfigRequest(BaseModel):
+    updated: Optional[Dict[str, Any]]
+    deleted: Optional[List[str]]
+
+class UpdateConfigResponse(BaseModel):
+    configs: Dict[str, Any]
+
+class UserDTO(BaseModel):
+    id: str  # unique id for the user
+    name: Optional[str]  # display name for the user
+    tenant_id: Optional[str]  # tenant id for the user
+    org_id: Optional[str]  # org id for the user
+    variables: Optional[Dict[str, Any]]  # variables for the user
+    roles: Optional[List[str]] = []  # roles for the user
+
+class CreateUserRequest(BaseModel):
+    user: UserDTO
+
+class UpdateUserRequest(BaseModel):
+    user: UserDTO
+
+class DeleteUserRequest(BaseModel):
+    id: str
+
+class CommonResponse(BaseModel):
+    pass
+
+class ListUsersRequest(BaseModel):
+    lookup_org_id: Optional[str]
+
+class ListUsersResponse(CommonResponse):
+    users: List[UserDTO]
+
+
+
+
+
 class UserImpl:
 
     def __init__(self, http_client: WaiiHttpClient):
@@ -46,6 +103,25 @@ class UserImpl:
 
     def delete_access_key(self, params:DelAccessKeyRequest):
         return self.http_client.common_fetch(DELETE_ACCESS_KEY_ENDPOINT, params.__dict__,DelAccessKeyResponse)
+
+    def get_user_info(self, params:GetUserInfoRequest):
+        return self.http_client.common_fetch(GET_USER_INFO_ENDPOINT, params.__dict__,GetUserInfoResponse)
+
+    def update_config(self, params:UpdateConfigRequest):
+        return self.http_client.common_fetch(UPDATE_CONFIG_ENDPOINT, params.__dict__,UpdateConfigResponse)
+
+    def create_user(self, params:CreateUserRequest):
+        return self.http_client.common_fetch(CREATE_USER_ENDPOINT, params.__dict__,CommonResponse)
+
+    def delete_user(self, params:DeleteUserRequest):
+        return self.http_client.common_fetch(DELETE_USER_ENDPOINT, params.__dict__, CommonResponse)
+
+    def update_user(self, params:UpdateUserRequest):
+        return self.http_client.common_fetch(UPDATE_USER_ENDPOINT, params.__dict__, CommonResponse)
+
+    def list_users(self, params:ListUsersRequest):
+        return self.http_client.common_fetch(LIST_USERS_ENDPOINT, params.__dict__, ListUsersResponse)
+
 
 
 User = UserImpl(WaiiHttpClient.get_instance())
